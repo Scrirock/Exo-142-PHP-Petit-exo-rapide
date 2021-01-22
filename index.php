4c8107ebfad8c01b5d156221e6442f3be527c578
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-22 14:43:59 +0100

    fg

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+    echo "ceci est un message";
+
+    echo "<br><br>";
+
+    $text = "un autre message";
+    echo $text;
+
+    echo "<br><br>";
+
+    $array = ["message", "toussa", "plein de truc", "des autre truc"];
+    echo $array[2];
+
+    echo "<br><br>";
+
+    for ($i = 0; $i<=3; $i++) echo $array[$i]."<br>";
+
+    echo "<br><br>";
+
+?>
+
+    <ul>
+        <li>zghftgh</li>
+        <li>fdztxthfxth</li>
+        <li>zdfhdtgh</li>
+        <li>dzsgdtrfgsdz</li>
+        <li>gadgdrgtg</li>
+    </ul>
+
+<?php
+
+    foreach ($array as $value) echo $value."<br>";
+
+?>
